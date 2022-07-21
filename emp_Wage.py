390cb5f5e4caa73625c365e3f6b93d8e789af36d
puja <pujarana1796@gmail.com>
2022-07-21 22:02:32 +0530

    puja added max hours and days of employees

--- emp_Wage.py
+++ emp_Wage.py
@@ -1,13 +1,16 @@
 from random import Random
 
 
-def MonthlyWage():
+def totalWage():
     IS_PART_TIME = 1
     IS_FULL_TIME = 2
     EMP_RATE_PER_HOUR = 20
-    NUM_OF_WORKING_DAYS = 20
-    totalEmpWage = 0
-    for day in range(0, NUM_OF_WORKING_DAYS):
+    NUM_OF_WORKING_DAYS = 2
+    MAX_HRS_IN_MONTH = 10
+    totalempHrs = 0
+    totalWorkingDays = 0
+    while totalempHrs <= MAX_HRS_IN_MONTH and totalWorkingDays < NUM_OF_WORKING_DAYS:
+        totalWorkingDays += 1
         random = Random()
         empCheck = random.randint(0, 3)
         if empCheck == IS_PART_TIME:
@@ -16,13 +19,14 @@ def MonthlyWage():
             empHrs = 8
         else:
             empHrs = 0
-        empWage = empHrs * EMP_RATE_PER_HOUR
-        totalEmpWage += empWage
-        print("Emp Wage :" + str(empWage))
-    print("Total Emp Wage :" + str(totalEmpWage))
-    return empWage
+
+        totalempHrs += empHrs
+        print("Day#:" + str(totalWorkingDays) + "empHrs : " + str(empHrs))
+    totalempWage = totalempHrs * EMP_RATE_PER_HOUR
+    print("totalempWage :" + str(totalempWage))
+    return totalempWage
 
 
 if __name__ == "__main__":
 
-    MonthlyWage()
+    totalWage()
