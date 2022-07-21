a88c7a2e07ec7fc8edfab4311ac25eaa792dfab7
puja <pujarana1796@gmail.com>
2022-07-21 16:12:27 +0530

    puja added program for parttime wage of employess

--- emp_Wage.py
+++ emp_Wage.py
@@ -1,20 +1,23 @@
 from random import Random
 
 
-def Dailywage():
-    IS_FULL_TIME = 1
+def partTime():
+    IS_FULL_TIME = 2
     EMP_RATE_PER_HOUR = 20
+    IS_PART_TIME = 1
     random = Random()
-    # computation
-    empCheck = random.randint(0, 2)
-    if empCheck == IS_FULL_TIME:
+    empCheck = random.randint(0, 3)
+    if empCheck == IS_PART_TIME:
+        empHrs = 4
+    elif empCheck == IS_FULL_TIME:
         empHrs = 8
     else:
         empHrs = 0
     empWage = empHrs * EMP_RATE_PER_HOUR
     print("Emp Wage :" + str(empWage))
+
     return empWage
 
 
 if __name__ == "__main__":
-    Dailywage()
+    partTime()
