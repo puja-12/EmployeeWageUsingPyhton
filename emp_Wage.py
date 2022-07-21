42a62056726b1320106127687acf627c56ddfe16
puja <pujarana1796@gmail.com>
2022-07-21 16:52:24 +0530

    puja added program for monthly wage of employees

--- emp_Wage.py
+++ emp_Wage.py
@@ -1,23 +1,28 @@
 from random import Random
 
 
-def partTime():
+def MonthlyWage():
+    IS_PART_TIME = 1
     IS_FULL_TIME = 2
     EMP_RATE_PER_HOUR = 20
-    IS_PART_TIME = 1
-    random = Random()
-    empCheck = random.randint(0, 3)
-    if empCheck == IS_PART_TIME:
-        empHrs = 4
-    elif empCheck == IS_FULL_TIME:
-        empHrs = 8
-    else:
-        empHrs = 0
-    empWage = empHrs * EMP_RATE_PER_HOUR
-    print("Emp Wage :" + str(empWage))
-
+    NUM_OF_WORKING_DAYS = 20
+    totalEmpWage = 0
+    for day in range(0, NUM_OF_WORKING_DAYS):
+        random = Random()
+        empCheck = random.randint(0, 3)
+        if empCheck == IS_PART_TIME:
+            empHrs = 4
+        elif empCheck == IS_FULL_TIME:
+            empHrs = 8
+        else:
+            empHrs = 0
+        empWage = empHrs * EMP_RATE_PER_HOUR
+        totalEmpWage += empWage
+        print("Emp Wage :" + str(empWage))
+    print("Total Emp Wage :" + str(totalEmpWage))
     return empWage
 
 
 if __name__ == "__main__":
-    partTime()
+
+    MonthlyWage()
