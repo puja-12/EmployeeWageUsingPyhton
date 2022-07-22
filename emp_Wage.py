b2ae135703f373d6c3919f51af00e15956d05ec8
puja <pujarana1796@gmail.com>
2022-07-22 15:57:47 +0530

    puja added daily wage along with total wage

--- emp_Wage.py
+++ emp_Wage.py
@@ -1,37 +1,28 @@
 from random import Random
 
 
-def employeeWage():
-    empwageperhrs = 20
-    emphrs = 0
-    workingdays = 0
-
-    ispresent = 1
-    random = Random()
-
-    empCheck = random.randint(0, 2)
-    if empCheck is ispresent:
-        print("employee is present")
-
-    else:
-        print("employee is absent")
-
-    if empCheck is 1:
-        emphrs += 8
-    elif empCheck is 2:
-        emphrs += 4
-    elif empCheck is 0:
-
-        emphrs = 0
-    else:
-        print("error")
-    workingdays += 1
-
-    print("Total Working Hours : " + str(emphrs))
-    print("Total working Days Per Month : " + str(workingdays))
-    monthsalary = empwageperhrs * emphrs
-    print("Total Salary of Employee per Month : " + str(monthsalary))
+def emplWage():
+    IS_PART_TIME = 1
+    IS_FULL_TIME = 2
+    EMP_RATE_PER_HOUR = 20
+    NUM_OF_WORKING_DAYS = 20
+    totalEmpWage = 0
+    for day in range(0, NUM_OF_WORKING_DAYS):
+        random = Random()
+        empCheck = random.randint(0, 3)
+        if empCheck is IS_PART_TIME:
+            empHrs = 4
+        elif empCheck is IS_FULL_TIME:
+            empHrs = 8
+        else:
+            empHrs = 0
+        empWage = empHrs * EMP_RATE_PER_HOUR
+        totalEmpWage += empWage
+        print("Emp Wage :" + str(empWage))
+    print("Total Emp Wage :" + str(totalEmpWage))
+    return empWage
 
 
 if __name__ == "__main__":
-    employeeWage()
+
+    emplWage()
