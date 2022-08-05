1a53d00124abd3c6e77a56e2f9519f84fbee0462
puja <pujarana1796@gmail.com>
2022-08-05 13:54:38 +0530

    puja added total wage per company

--- emp_Wage.py
+++ emp_Wage.py
@@ -2,16 +2,22 @@ from random import Random
 
 
 class TotalSalary:
-
-    def __init__(self):
-
+    def __init__(self, Company, wagePerHour, maxWorkingDays, maxWorkingHours):
+        # instance fields found by C# to Python Converter:
         self._wagePerHour = 20
         self._workingHours = 0
         self._workingDaysPerMonth = 0
         self._maxWorkingDays = 0
         self._maxWorkingHours = 0
+        self._Company = None
+        self._totalSalary = 0
 
-    def salary(self, company, wagePerHour, maxWorkingDays, maxWorkingHours):
+        self._Company = Company
+        self._wagePerHour = wagePerHour
+        self._maxWorkingDays = maxWorkingDays
+        self._maxWorkingHours = maxWorkingHours
+
+    def salary(self):
 
         attendanceCheck = Random()
         isPresent = attendanceCheck.randint(0, 4)
@@ -27,29 +33,27 @@ class TotalSalary:
             print("Employee is Absent!")
             # WorkingHours = 0
 
-        while (self._workingHours < maxWorkingHours) and (self._workingDaysPerMonth < maxWorkingDays):
+        while (self._workingHours < self._maxWorkingHours) and (self._workingDaysPerMonth < self._maxWorkingDays):
 
             if isPresent == 1:
-
+                # Console.WriteLine("Employee is present and working Full Time!")
                 self._workingHours += 8
             elif isPresent == 2:
-
+                # Console.WriteLine("Employee is present and working Part Time!")
                 self._workingHours += 4
             elif isPresent == 0:
-
+                # Console.WriteLine("Employee is Absent!")
                 self._workingHours = 0
-
-        self._workingDaysPerMonth += 1
+                # Console.WriteLine("Something went wrong!!")
+            self._workingDaysPerMonth += 1
         print("Total Working Hours : " + str(self._workingHours))
         print("Total working Days Per Month : " + str(self._workingDaysPerMonth))
-        totalSalary = wagePerHour * self._workingHours
-        print("Total Salary of Employee per Month ", company, totalSalary)
+        totalSalary = self._wagePerHour * self._workingHours
+        print("Total Salary of Employee per Month ", self._Company, "is", totalSalary)
 
 
 if __name__ == "__main__":
-    total = TotalSalary()
-
-    print("Welcome to Employ Wage Computation Program!")
-    total.salary("Amazon", 20, 20, 100)
-    total.salary("Bridgelabz", 18, 21, 80)
-    total.salary("TCS", 19, 24, 90)
+    Amazon = TotalSalary("Amazon", 20, 5, 100)
+    Amazon.salary()
+    BridgeLabs = TotalSalary("Bridge Labs", 25, 5, 120)
+    BridgeLabs.salary()
