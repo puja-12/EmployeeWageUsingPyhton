eff635028e7d35dfa6bd0d27accbb4acf3383923
puja <pujarana1796@gmail.com>
2022-08-05 13:56:15 +0530

    puja added total wage per company

--- emp_Wage.py
+++ emp_Wage.py
@@ -2,17 +2,16 @@ from random import Random
 
 
 class TotalSalary:
-    def __init__(self, Company, wagePerHour, maxWorkingDays, maxWorkingHours):
+    def __init__(self, wagePerHour, maxWorkingDays, maxWorkingHours, company):
         # instance fields found by C# to Python Converter:
         self._wagePerHour = 20
         self._workingHours = 0
         self._workingDaysPerMonth = 0
         self._maxWorkingDays = 0
         self._maxWorkingHours = 0
-        self._Company = None
         self._totalSalary = 0
 
-        self._Company = Company
+        self._company = company
         self._wagePerHour = wagePerHour
         self._maxWorkingDays = maxWorkingDays
         self._maxWorkingHours = maxWorkingHours
@@ -36,20 +35,20 @@ class TotalSalary:
         while (self._workingHours < self._maxWorkingHours) and (self._workingDaysPerMonth < self._maxWorkingDays):
 
             if isPresent == 1:
-                # Console.WriteLine("Employee is present and working Full Time!")
+
                 self._workingHours += 8
             elif isPresent == 2:
-                # Console.WriteLine("Employee is present and working Part Time!")
+
                 self._workingHours += 4
             elif isPresent == 0:
-                # Console.WriteLine("Employee is Absent!")
+
                 self._workingHours = 0
-                # Console.WriteLine("Something went wrong!!")
+
             self._workingDaysPerMonth += 1
         print("Total Working Hours : " + str(self._workingHours))
         print("Total working Days Per Month : " + str(self._workingDaysPerMonth))
         totalSalary = self._wagePerHour * self._workingHours
-        print("Total Salary of Employee per Month ", self._Company, "is", totalSalary)
+        print("Total Salary of Employee per Month ", self._company, "is", totalSalary)
 
 
 if __name__ == "__main__":
