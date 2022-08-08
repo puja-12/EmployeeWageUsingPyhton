b1f88d3c549c73119fdea58dc6021427b0bf042c
puja <pujarana1796@gmail.com>
2022-08-08 18:57:40 +0530

    puja added CRUD operation

--- emp_Wage.py
+++ emp_Wage.py
@@ -1,54 +1,51 @@
 from random import Random
 
 
-class TotalSalary:
+class Employee:
 
-    def __init__(self, company, wagePerHour, maxWorkingDays, maxWorkingHours):
+    def __init__(self, data: dict):
+        self.com_name = data.get("company_name")
+        self.emp_name = data.get("emp_name")
+        self.emp_id = data.get("emp_id")
+        self.emp_salary = data.get("emp_salary")
+        self.emp_phone = data.get("emp_phone")
+        print(data)
 
-        self._company = company
-        self._wagePerHour = wagePerHour
-        self._maxWorkingDays = maxWorkingDays
-        self._maxWorkingHours = maxWorkingHours
+    # def details(self):
+    #     return f"{self.company_name},{self.wagePerHour},{self.maxWorkingDays},{self.maxWorkingHours}"
 
-    def salary(self):
-        workingHours = 0
-        workingDaysPerMonth = 0
 
-        attendanceCheck = Random()
-        isPresent = attendanceCheck.randint(0, 4)
-        print(isPresent)
-        if isPresent == 1:
-            print("Employee is present and working Full Time!")
-            # WorkingHours = 8
-        elif isPresent == 2:
-            print("Employee is working for part Time!")
-            # WorkingHours = 4
-            # if isPresent is 2 then we consider it as part time and working hours are 4
-        else:
-            print("Employee is Absent!")
-            # WorkingHours = 0
+class Company:
+    def __init__(self):
+        self.emp_dict = {}
 
-        while (workingHours < self._maxWorkingHours) and (workingDaysPerMonth < self._maxWorkingDays):
+    def add_contacts(self, contact_object):
+        self.emp_dict.update({contact_object.company_name: contact_object})
 
-            if isPresent == 1:
 
-                workingHours += 8
-            elif isPresent == 2:
-
-                workingHours += 4
-            elif isPresent == 0:
-
-                workingHours = 0
-
-            workingDaysPerMonth += 1
-        print("Total Working Hours : " + str(workingHours))
-        print("Total working Days Per Month : " + str(workingDaysPerMonth))
-        totalSalary = self._wagePerHour * workingHours
-        print("Total Salary of Employee per Month ", self._company, "is", totalSalary)
+#     def all_values(self):
+#         """
+#             function to read all contacts from dictionary
+#             """
+#         # Iterate over all values of the dictionary
+#         for key, value in self.emp_dict.items():
+#             print(key, value.details())
 
 
 if __name__ == "__main__":
-    Amazon = TotalSalary("Amazon", 20, 5, 100)
-    Amazon.salary()
-    BridgeLabs = TotalSalary("Bridge Labs", 25, 5, 120)
-    BridgeLabs.salary()
+    # company = Company()
+    # print("Enter the company name :")
+    # com_name = input()
+    # print("Enter the emp_name :")
+    # emp_name = input()
+    # print("Enter the emp_id :")
+    # emp_id = input()
+    # print("Enter the emp_salary :")
+    # emp_salary = input()
+    # print("Enter the emp_phone :")
+    # emp_phone = input()
+    # emp = Employee ()
+    # company.add_contacts()
+    emp = Employee("company_name"="amazon")
+
+
